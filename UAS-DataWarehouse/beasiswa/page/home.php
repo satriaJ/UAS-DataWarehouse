626a047ed2b24a6ef15fb3be8167b4a685abedeb
satriaJ <91950252+satriaJ@users.noreply.github.com>
2022-01-09 22:44:32 +0700

    Add files via upload

---
+++ UAS-DataWarehouse/beasiswa/page/home.php
@@ -0,0 +1,10 @@
+<body bgcolor="Pink">
+<h1><p align="center">
+<font color="white">
+   	Selamat Datang Calon Penerima Beasiswa <br>
+	DIV Teknik Informatika <br>
+	Tahun Ajaran 2020/2021
+</h1> 
+</b>
+</p>
+</font>
